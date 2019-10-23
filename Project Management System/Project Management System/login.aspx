d76f96f652cac2344716860872b3b0a852cca549
aritra <aritrabasu1999@gmail.com>
2019-10-23 10:18:57 +0000

    work so far

---
+++ Project Management System/Project Management System/login.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="Project_Management_System.login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="login.css" />
+</head>
+<body>
+    <div class="login">
+  <div class="heading">
+    <h2>Sign in</h2>
+    <form runat="server" action="#">
+    <asp:Label ID="Label1" runat="server"></asp:Label>
+      <div class="input-group input-group-lg">
+        <span class="input-group-addon"><i class="fa fa-user"></i></span>
+        <asp:TextBox ID="Login_username" runat="server" textmode="SingleLine" CSSclass="form-control" placeholder="Username or email" />
+          </div>
+        <div class="input-group input-group-lg">
+          <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+          <asp:TextBox ID="Login_password" runat="server" textmode="password" CSSclass="form-control" placeholder="Password" />
+        </div>
+
+        <asp:Button ID="Login_button" runat="server" CSSclass="float" Text="Login" OnClick="Login_Click"></asp:Button>
+       </form>
+ 		</div>
+ </div>
+</body>
+</html>
---
+++ Project Management System/Project Management System/login.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Data.SqlClient;
+using System.Data;
+using System.Security.Cryptography;
+using System.IO;
+using System.Text;
+using System.Web.UI.WebControls;
+
+namespace Project_Management_System
+{
+    public partial class login : System.Web.UI.Page
+    {
+        private String connection = "server=localhost;user id=root; password=root;persistsecurityinfo=True;database=project";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Login_Click(object sender, EventArgs e)
+        {
+            var query = "Select password from users where username = @username";
+            using (var conn = new SqlConnection(connection))
+            {
+                using (var cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.Add("@username", SqlDbType.VarChar);
+                    cmd.Parameters["@username"].Value = Convert.ToString(Login_username.Text);
+                    try
+                    {
+                        conn.Open();
+                    }
+                    catch (SqlException ex)
+                    {
+                        Label1.Text+=$"Can not open connection ! ErrorCode: {ex.ErrorCode} Error: {ex.Message}";
+                    }
+                    catch (Exception ex)
+                    {
+                        Label1.Text+=$"Can not open connection ! Error: {ex.Message}";
+                    }
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if(!reader.HasRows)
+                    {
+                        Response.Redirect("./login.aspx");
+                    }
+                    else
+                    {
+                        string password = (string)reader["password"];
+                        string salt = (string)reader["salt"];
+                        HashAlgorithm sha = new SHA1CryptoServiceProvider();
+                        string hashed = Convert.ToBase64String(sha.ComputeHash(Encoding.ASCII.GetBytes(Login_password.Text)));
+                        if (password == hashed)
+                        {
+                            Response.Redirect("./dashboard.aspx");
+                        }
+                        else
+                        {
+                            Response.Redirect("./login.aspx");
+                        }
+                    }
+                }
+            }
+            return;
+        }
+    }
+}
